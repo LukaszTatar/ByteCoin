83475a9c6a64344b024a42a780a8c862c37b4a0b
Lukasz Tatarynowicz <tatarynowicz92@gmail.com>
2019-12-04 20:38:00 +0100

    final commit, app working properly

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Łukasz Tatarynowicz on 03/12/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let last: Double
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,9 +8,47 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateBitcoin(_ coinManager: CoinManager, coin: Double, currency: String)
+    func didFailWithError (_ error: Error)
+}
+
 struct CoinManager {
     
+    var delegate: CoinManagerDelegate?
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
+    func getCoinPrice(for currency: String) {
+    
+        let urlString = "\(baseURL)\(currency)"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error!)
+                    return
+                }
+                if let safeData = data {
+                    if let bitcoinPrice = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateBitcoin(self, coin: bitcoinPrice, currency: currency)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    func parseJSON(_ coinData: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: coinData)
+            let lastPrice = decodedData.last
+            print(lastPrice)
+            return lastPrice
+        } catch {
+            delegate?.didFailWithError(error)
+            return nil
+        }
+    }
 }
